03835dbb17da48752323acc8a5efb75a62836761
Shannon Mettry <shannonmettry12@gmail.com>
2022-07-05 21:58:17 +0200

    changed nav set upon every page to make it more consistent and faster

--- nav.js
+++ nav.js
@@ -1,8 +1,17 @@
-fetch('nav.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#nav");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
+const createNav = () => {
+    let nav = document.querySelector('nav');
+
+    nav.innerHTML = `
+    <nav id="nav" class="nav">
+    <!--     <div class="logo"><a href="index.html">K & F</a></div> --> 
+          <ul class="nav-ul">
+            <li><a class="nav-links" href="index.html">Home</a></li><span class="line">|</span> 
+            <li><a class="nav-links" href="boda.html">Boda</a></li><span class="line">|</span>
+            <li><a class="nav-links" href="recs.html">Recomendaciones</a></li><span class="line">|</span>
+            <li><a class="nav-links" href="regalo.html">Regalo</a></li><span class="line">|</span>
+            <li><a class="nav-links" href="rsvp.html">RSVP</a></li>
+          </ul>
+        </nav>`
+}
+
+createNav();
