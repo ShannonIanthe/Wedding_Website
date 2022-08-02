d0cb0edf30773e23075f0f10404b90b751b3c271
Shannon Mettry <shannonmettry12@gmail.com>
2022-08-02 23:32:03 +0200

    fixing the k&f on navbar in Boda page

--- nav.js
+++ nav.js
@@ -5,7 +5,7 @@ const createNav = () => {
     <nav id="nav" class="nav">
     <!--     <div class="logo"><a href="index.html">K & F</a></div> --> 
           <ul class="nav-ul">
-            <li><a class="nav-links" href="index.html">K & F</a></li><span class="line">|</span> 
+            <li><a class="nav-links" href="index.html">K&F</a></li><span class="line">|</span> 
             <li><a class="nav-links" href="boda.html">Boda</a></li><span class="line">|</span>
             <li><a class="nav-links" href="regalo.html">Regalo</a></li><span class="line">|</span>
             <li><a class="nav-links" href="rsvp.html">RSVP</a></li>
