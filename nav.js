fcc134afb6563831b608ca308a539dc550a96768
Shannon Mettry <shannonmettry12@gmail.com>
2022-09-04 16:51:45 +0200

    first commit

--- nav.js
+++ nav.js
@@ -3,9 +3,9 @@ const createNav = () => {
 
     nav.innerHTML = `
     <nav id="nav" class="nav">
-    <!--     <div class="logo"><a href="index.html">K & F</a></div> --> 
+    <!--     <div class="logo"><a href="index.html">O & M</a></div> --> 
           <ul class="nav-ul">
-            <li><a class="nav-links" href="index.html">K&F</a></li><span class="line">|</span> 
+            <li><a class="nav-links" href="index.html">O&M</a></li><span class="line">|</span> 
             <li><a class="nav-links" href="boda.html">Boda</a></li><span class="line">|</span>
             <li><a class="nav-links" href="regalo.html">Regalo</a></li><span class="line">|</span>
             <li><a class="nav-links" href="rsvp.html">RSVP</a></li>
