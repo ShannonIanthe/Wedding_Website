20b17176c8de821dd692068465bb4f72aa3fa165
Shannon Mettry <shannonmettry12@gmail.com>
2022-09-26 15:56:58 +0200

    made animation changes

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+gsap.from(".hero", {
+    opacity: 0,
+    y: 100,
+    duration: 6,
+  });
+
+
+  gsap.from(".regalo-container", {
+    opacity: 0,
+    y: 100,
+    duration: 6,
+  });
+ 
+ 
+;
